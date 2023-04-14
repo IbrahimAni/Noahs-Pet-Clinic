bb726c58ca7d2f7dcc6c35d061f5c3b4a2083286
Hopizi <55993084+Hopizi@users.noreply.github.com>
2023-04-14 21:45:03 +0100

    completed the responsive landing page

--- np-clinc/src/sections/Navbar.jsx
+++ np-clinc/src/sections/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import "./styles/Navbar.css"
 import { Button } from '../components';
+import { useState, useEffect, useRef } from 'react';
 
 const Navbar = () => {
+    const menuRef = useRef(null)
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    function handleClick() {
+      setMenuOpen(!menuOpen);
+    }
+
+    const menuClass = menuOpen ? "open" : "" ;
+    const showNav = menuOpen ? "showNav" : "" ;
+    const navCon = menuOpen ? "navCon" : "" ;
+ 
   return (
     <div className='navbar-conatiner'>
       <div className='nav-items-container'>
         <div className='company-logo'>
           <h2>Noah's.</h2>
         </div>
-        <div className='navbar-items'>
-            <ul>
+        <div className={`navbar-items ${navCon}`}>
+            <ul className={`nav-ul-list ${menuOpen ? "showNav" : ""}`}>
               <li>Home</li>
               <li>About</li>
               <li>Services</li>
@@ -18,14 +31,17 @@ const Navbar = () => {
               <li>Appointments</li>
               <li>Contact</li>
               <li>Blog</li>
+              <li className={`login-nav-not ${menuOpen ? "login-show" : ""}`}>Login</li>
             </ul>
         </div>
         <div className='navbar-login-btn'>
             <Button name="Login" btnClass="btn-login"/>
         </div>
-        <div className='nav-btn-container'>
+        <div className={`nav-btn-container ${menuClass}`} 
+        onClick={handleClick}
+        >
           <div className='menu-btn'>
-            
+
           </div>
         </div>
       </div>
